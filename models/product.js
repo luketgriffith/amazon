17b5ef6b71f886970f9f8250ce8b820f741a1465
Luke Griffith <luketgriffith@gmail.com>
2016-02-08 22:32:18 -0500

    searching and pagination

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
@@ -7,6 +8,11 @@ var ProductSchema = new Schema({
   price: Number,
   image: String
 
+});
+ProductSchema.plugin(mongoosastic, {
+  hosts: [
+    'localhost:9200'
+  ]
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
--- routes/main.js
+++ routes/main.js
@@ -1,18 +1,116 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var Product = require('../models/product');
+
+function paginate(req, res, next) {
+  var perPage = 9;
+    var page = req.params.page;
+
+    Product
+      .find()
+      .skip( perPage * page) //skips documents already viewed in DB
+      .limit( perPage )
+      .populate('category')
+      .exec(function(err, products) {
+        if(err) return next(err);
+        Product.count().exec(function(err, count){
+          if(err) return next(err);
+          res.render('main/product-main', {
+            products: products,
+            pages: count / perPage
+          });
+        });
+      });
 
 
-router.get('/', function(req, res) {
-  console.log('meow');
-  res.render('main/home');
+}
+
+
+
+
+Product.createMapping(function(err, mapping){
+  if(err){
+    console.log('error mapping');
+    console.log(err);
+  } else {
+    console.log('mapping created');
+    console.log(mapping);
+  }
 });
+
+var stream = Product.synchronize();
+var count = 0;
+
+stream.on('data', function() {
+  count++;
+});
+stream.on('close', function() {
+  console.log('indexed' + count + 'documents');
+});
+stream.on('error', function(err) {
+  console.log(err)
+});
+
+router.post('/search', function(req, res, next){
+  res.redirect('/search?q='+ req.body.q);
+});
+
+router.get('/search', function(req, res, next){
+  if(req.query.q){
+    Product.search({
+      query_string: { query: req.query.q }
+    }, function(err, results) {
+      if(err) return next(err);
+      var data = results.hits.hits.map(function(hit) {
+        return hit;
+      });
+
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+
+    });
+  }
+});
+
+router.get('/', function(req, res, next) {
+  if(req.user){
+    paginate(req, res, next);
+
+  } else{
+
+    res.render('main/home');
+  }
+});
+
+router.get('/page/:page', function(req, res, next) {
+  paginate(req, res, next);
+});
+
 router.get('/about', function(req, res){
   res.render('main/about');
 });
-router.get('/users', function(req, res){
-  User.find({}, function(err, users){
-    res.json(users)
-  })
-})
+
+router.get('/products/:id', function(req, res, next){
+  Product 
+    .find({ category: req.params.id})
+    .populate('category')
+    .exec(function(err, products){
+      if (err) return next(err);
+      res.render('main/category', {
+        products: products
+      });
+    });
+});
+
+router.get('/product/:id', function(req, res, next) {
+  Product.findById({ _id: req.params.id}, function(err, product) {
+    if(err) return next(err);
+    res.render('main/product', {
+      product: product
+    });
+  });
+});
 
 module.exports = router;
